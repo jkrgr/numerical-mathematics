288fdb127a4a96b9d79bc39da7bddde22d278229
jk <jk@redpai.no>
2012-10-12 18:20:44 +0200

    Cleaned up some code, fixed references and appendices. Project done.

--- project-2/createQ2.m
+++ project-2/createQ2.m
@@ -1,16 +1,17 @@
 %Builds the A(nxn) matrices
-function Q2 = createQ2(n)
-Q2=zeros(n);
 
-for i = 1:n
-    for j=1:n
-        if i==j
-            Q2(i,j)=-2;
-        elseif abs(i-j)==1
-            Q2(i,j)= 1;
+function Q2 = createQ2(n)
+    Q2 = zeros(n);
+
+    for i = 1:n
+        for j=1:n
+            if i==j
+                Q2(i,j) = -2;
+            elseif abs(i-j)==1
+                Q2(i,j) = 1;
+            end
         end
     end
-end
 
-Q2=eye(n)-(n^2)*Q2;
+    Q2 = eye(n)-(n^2)*Q2;
 end
--- project-2/sqrtiterate.m
+++ project-2/sqrtiterate.m
@@ -1,13 +1,15 @@
 function list = sqrtiterate(A,times)
-Y=eye(length(A)); %Identity matrix
-root=eigroot(A); %Solution of the root of A
-list=zeros(times+1,1); %Creates an empty vector to store max norms
-maxnorm=max(max(abs(root-Y)));
-list(1)=maxnorm; 
-for x = 1:times
-    Y=0.5*(Y+Y\A); %Calculates each iteration
-    maxnorm=max(max(abs(root-Y))); %Calculates the error
-    list(x+1)=maxnorm; %Adds it to the vector
-end
-
+    %Identity matrix
+    Y = eye(length(A));
+    %Solution of the root of A
+    root = eigroot(A);
+    %Creates an empty vector to store max norms
+    list = zeros(times+1,1);
+    maxnorm = max(max(abs(root-Y)));
+    list(1) = maxnorm;
+    for x = 1:times
+        Y = 0.5*(Y+Y\A);                 %Calculates each iteration
+        maxnorm = max(max(abs(root-Y))); %Calculates the error
+        list(x+1) = maxnorm;             %Adds it to the vector
+    end
 end
