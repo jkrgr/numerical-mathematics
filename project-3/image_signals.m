63cd19ca61a06952e7bdc1b57bd01de186ab8b63
Joachim Kruger <joachibe@null05.math.ntnu.no>
2012-11-11 15:50:59 +0100

    Calculate kurtosis

--- project-3/image_signals.m
+++ project-3/image_signals.m
@@ -2,6 +2,12 @@
 spock = double(imread('spock.jpg'));
 r2d2 = double(imread('r2d2.jpg'));
 bender = double(imread('bender.jpg'));
+
+% Ensure that the kurtosis is positive
+kurtosis(spock(:))
+kurtosis(r2d2(:))
+kurtosis(bender(:))
+
 % Plot the superposition of the signals
 imagesc(spock+r2d2+bender)
 colormap gray
